674075f6943487ac8aa561512af514c0d4b7a0d8
Yoshida-yuki <delicious-a@ezweb.ne.jp>
2020-07-01 20:52:47 +0900

    レダイレクト設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,6 +22,7 @@ class PostsController < ApplicationController
   def update
     @post = Post.find(params[:id])
     post.update(post_params)
+    redirect_to post_path(post.id)
   end
 
   private
