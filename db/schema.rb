44bd18a6bbd4b37bf8ec31c08442839eb6e4cc9e
Yoshida-yuki <delicious-a@ezweb.ne.jp>
2020-07-09 15:17:36 +0900

    devise実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_21_112437) do
+ActiveRecord::Schema.define(version: 2020_07_09_061653) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
 end
